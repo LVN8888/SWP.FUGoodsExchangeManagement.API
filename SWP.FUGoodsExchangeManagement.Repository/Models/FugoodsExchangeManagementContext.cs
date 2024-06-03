10641521b8a41d59de475499434515d713004444
PhuHoang07 <duongphu1207@gmail.com>
2024-06-03 21:12:25 +0700

    + ScriptDB

--- SWP.FUGoodsExchangeManagement.Repository/Models/FugoodsExchangeManagementContext.cs
+++ SWP.FUGoodsExchangeManagement.Repository/Models/FugoodsExchangeManagementContext.cs
@@ -41,10 +41,6 @@ public partial class FugoodsExchangeManagementContext : DbContext
 
     public virtual DbSet<User> Users { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=fugoodsexchange.database.windows.net;uid=fuadmin01;pwd=AdmPass01;database=FUGoodsExchangeManagement;TrustServerCertificate=True");
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Campus>(entity =>
