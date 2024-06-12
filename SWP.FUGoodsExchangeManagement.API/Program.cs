f9a92835874eb8328809fbecfc92c5ba1a3425b7
Fus <79319323+nokfur@users.noreply.github.com>
2024-06-12 16:01:59 +0700

    Update Program.cs

--- SWP.FUGoodsExchangeManagement.API/Program.cs
+++ SWP.FUGoodsExchangeManagement.API/Program.cs
@@ -106,11 +106,11 @@ builder.Services.AddSwaggerGen(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 // Add middleware
 app.UseMiddleware<GlobalExceptionMiddleware>();
