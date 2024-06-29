358c10fa04b2f805999ac921ea11d29614028d1b
Fu <phunnse171674@fpt.edu.vn>
2024-06-29 13:05:38 +0700

    [update] use key vault for connection string

---
+++ SWP.FUGoodsExchangeManagement.Business/Service/SecretServices/SecretService.cs
@@ -0,0 +1,34 @@
+﻿using Azure.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Security.KeyVault.Secrets;
+
+namespace SWP.FUGoodsExchangeManagement.Business.Service.SecretServices
+{
+    public static class SecretService
+    {
+        public static readonly string ConnectionString;
+
+        private const string KeyVaultURI = "https://fugoodsexchangekeyvault.vault.azure.net/";
+        private static readonly Uri keyVaultEndPoint;
+        private static readonly SecretClient secretClient;
+
+        static SecretService()
+        {
+            keyVaultEndPoint = new Uri(KeyVaultURI);
+            secretClient = new SecretClient(keyVaultEndPoint, new DefaultAzureCredential());
+
+            ConnectionString = GetConnectionString();
+        }
+
+        private static string GetConnectionString()
+        {
+            KeyVaultSecret keyVaultSecret = secretClient.GetSecret("FuGoodsExchangeConnectionString");
+
+            return keyVaultSecret.Value;
+        }
+    }
+}
