c37e3bc413f585bded37738b3459e550ca223ea2
Lê Văn Nguyên <levannguyen2003@gmail.com>
2024-07-09 08:27:24 +0700

    [Update] Fix regex of fullname in edit infor user

--- SWP.FUGoodsExchangeManagement.Repository/DTOs/UserDTOs/RequestModels/UserEditRequestModel.cs
+++ SWP.FUGoodsExchangeManagement.Repository/DTOs/UserDTOs/RequestModels/UserEditRequestModel.cs
@@ -15,7 +15,7 @@ namespace SWP.FUGoodsExchangeManagement.Repository.DTOs.UserDTOs.RequestModels
         public string Id { get; set; }
         [Required]
         [StringLength(100, MinimumLength = 2, ErrorMessage = "Fullname must be between 2 and 100 characters.")]
-        [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Fullname can only contain letters and spaces.")]
+        [RegularExpression(@"^[\p{L}\s]+$", ErrorMessage = "Fullname can only contain letters and spaces.")]
         public string Fullname { get; set; }      
 
         [StringLength(15)]
